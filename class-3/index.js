553ce2b8c8d528d42989e9603559919aaac62eba
mrinal1224 <iammrinalkc@gmail.com>
2024-03-30 22:11:21 +0530

    schema and model

--- class-3/index.js
+++ class-3/index.js
@@ -1,15 +1,54 @@
 const express = require("express");
 const fs = require("fs");
-
 const app = express();
-
+const mongoose = require("mongoose");
 const products = require("./MOCK_DATA.json");
 
+// DB Connection
+
+mongoose
+  .connect(
+    "mongodb+srv://mrinalbhattacharya:jrybX27vUUhJTnXs@cluster0.qkdrsui.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    console.log("db Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+
+
+// Schema for product
+
+const productSchema =  new mongoose.Schema({
+    product_name : {
+      type : String,
+      required :true 
+    },
+    product_price : {
+      type : String,
+      required : true
+    },
+
+    isInStock : {
+      type : Boolean,
+      required : true
+    },
+
+    category : {
+      type : String,
+      required : true
+    }
+
+})
+
+const ProductModel = mongoose.model('products' , productSchema)
 
 const PORT = 8006;
 
 app.use(express.urlencoded());
-app.use(express.json())
+app.use(express.json());
 
 // Routing
 
@@ -48,8 +87,8 @@ app.post("/api/products", (req, res) => {
   const newData = req.body;
   products.push({ id: products.length + 1, ...newData });
   fs.writeFile("./MOCK_DATA.json", JSON.stringify(products), (error, data) => {
-    if(err){
-        return res.send(error)
+    if (err) {
+      return res.send(error);
     }
     return res.send({ status: "succesfully created", id: products.length });
   });
@@ -59,55 +98,40 @@ app.listen(PORT, () => {
   console.log(`The server is running in port ${PORT}`);
 });
 
+// put
 
-// put 
+app.put("/api/products/:id", (req, res) => {
+  const id = Number(req.params.id); // 7
+  const body = req.body;
+  const productIndex = products.findIndex((product) => product.id === id);
 
-app.put('/api/products/:id' , (req , res)=>{
-    const id = Number(req.params.id); // 7
-     const body  = req.body
-    const productIndex = products.findIndex((product) => product.id === id);
+  products[productIndex] = { id: id, ...body };
 
-
-    products[productIndex] = {id:id , ...body }
-
-    fs.writeFile("./MOCK_DATA.json", JSON.stringify(products), err => {
-        if (err) {
-          return res.status(500).json({ status: "error", message: "Failed to update product" });
-        }
-        return res.json({ status: "updated successfully", id: id });
-      });
-
-})
-
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(products), (err) => {
+    if (err) {
+      return res
+        .status(500)
+        .json({ status: "error", message: "Failed to update product" });
+    }
+    return res.json({ status: "updated successfully", id: id });
+  });
+});
 
 //Delete
 
-app.delete('/api/products/:id' , (req , res)=>{
-    const id = Number(req.params.id); // 7
+app.delete("/api/products/:id", (req, res) => {
+  const id = Number(req.params.id); // 7
 
-    const productIndex = products.findIndex((product) => product.id === id);
+  const productIndex = products.findIndex((product) => product.id === id);
 
-    products.splice(productIndex , 1)
+  products.splice(productIndex, 1);
 
-    fs.writeFile("./MOCK_DATA.json", JSON.stringify(products), err => {
-        if (err) {
-          return res.status(500).json({ status: "error", message: "Failed to update product" });
-        }
-        return res.json({ status: "Deleted successfully" });
-      });
-
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(products), (err) => {
+    if (err) {
+      return res
+        .status(500)
+        .json({ status: "error", message: "Failed to update product" });
+    }
+    return res.json({ status: "Deleted successfully" });
+  });
+});
